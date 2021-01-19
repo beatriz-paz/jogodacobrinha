13b73800309ed490e3dbacc67981d9ae4672d3a8
beatriz-paz <estudo.bea@gmail.com>
2021-01-18 21:04:07 -0300

    criar cobrinha

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
+//criando a área do jogo:
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+//posição 0 é igual as posições X e Y
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);//desenha a área do jogo
 }
+//criando a cobrinha:
+
+function criarCobrinha(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
 
 criarBG();
+criarCobrinha();
+
 
