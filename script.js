1617bcc1e9912cde42e7ec9a2ecd786a7dfa3040
beatriz-paz <estudo.bea@gmail.com>
2021-01-19 07:29:39 -0300

    criando movimento da cobrinha

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ snake[0] = {
     y: 8 * box
 }
 
+//dando movimento para a cobrinha:
+
+let direction = "right";
+
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);//desenha a área do jogo
@@ -22,7 +26,31 @@ function criarCobrinha(){
     }
 }
 
-criarBG();
-criarCobrinha();
+function iniciarJogo(){
+    criarBG();
+    criarCobrinha();
+    //criar a posição de partida da cobrinha
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;   
+    
+    //passando condicionais de movimento para a cobrinha:
+    //se a direção for = "right" a posição do snekeX vai acrescentar um box (quadradinho)
+    if(direction =="right") snakeX += box;
+    //vamos diminur um box para entender que estamos indo para esquerda (pois é como no plano cartesiano)
+    if(direction =="left") snakeX -= box;
+    if(direction =="up") snakeY -= box;
+    if(direction =="down") snakeY += box;
+    //função que retira o ultimo elemento da cobrinha
+    snake.pop();
+    //criando a cabeça da cobrinha
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+//função de atualizar o jogo de tempos em tempos (100ms) para a cobrinha conseguir se mexer 
+let jogo = setInterval(iniciarJogo, 100);
 
 
